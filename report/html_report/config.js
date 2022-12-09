2d133f470427829966ff85e0248727ed5b41eee5
Tolik Petras <toliketras4@gmail.com>
2022-12-09 11:35:15 +0200

    make build

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document_1024px.png",
-        "test": "..\\bitmaps_test\\20221207-132920\\Entire_document_1024px.png",
+        "test": "..\\bitmaps_test\\20221209-113340\\Entire_document_1024px.png",
         "selector": "document",
         "fileName": "Entire_document_1024px.png",
         "label": "Entire document",
@@ -15,22 +15,20 @@ report({
         "expect": 0,
         "viewportLabel": "1024px",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
-            "height": -1
+            "height": 0
           },
-          "misMatchPercentage": "4.46",
-          "analysisTime": 54
-        },
-        "diffImage": "..\\bitmaps_test\\20221207-132920\\failed_diff_Entire_document_1024px.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document_720px.png",
-        "test": "..\\bitmaps_test\\20221207-132920\\Entire_document_720px.png",
+        "test": "..\\bitmaps_test\\20221209-113340\\Entire_document_720px.png",
         "selector": "document",
         "fileName": "Entire_document_720px.png",
         "label": "Entire document",
@@ -41,22 +39,20 @@ report({
         "expect": 0,
         "viewportLabel": "720px",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
-            "height": -1
+            "height": 0
           },
-          "misMatchPercentage": "4.77",
-          "analysisTime": 56
-        },
-        "diffImage": "..\\bitmaps_test\\20221207-132920\\failed_diff_Entire_document_720px.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Big_search_bar_data-qa_big_1024px.png",
-        "test": "..\\bitmaps_test\\20221207-132920\\Big_search_bar_data-qa_big_1024px.png",
+        "test": "..\\bitmaps_test\\20221209-113340\\Big_search_bar_data-qa_big_1024px.png",
         "selector": "[data-qa=\"big\"]",
         "fileName": "Big_search_bar_data-qa_big_1024px.png",
         "label": "Big search bar data-qa_big",
@@ -67,22 +63,20 @@ report({
         "expect": 0,
         "viewportLabel": "1024px",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
-            "height": -1
+            "height": 0
           },
-          "misMatchPercentage": "2.24",
-          "analysisTime": 18
-        },
-        "diffImage": "..\\bitmaps_test\\20221207-132920\\failed_diff_Big_search_bar_data-qa_big_1024px.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Big_search_bar_data-qa_big_720px.png",
-        "test": "..\\bitmaps_test\\20221207-132920\\Big_search_bar_data-qa_big_720px.png",
+        "test": "..\\bitmaps_test\\20221209-113340\\Big_search_bar_data-qa_big_720px.png",
         "selector": "[data-qa=\"big\"]",
         "fileName": "Big_search_bar_data-qa_big_720px.png",
         "label": "Big search bar data-qa_big",
@@ -93,22 +87,20 @@ report({
         "expect": 0,
         "viewportLabel": "720px",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
-            "height": -1
+            "height": 0
           },
-          "misMatchPercentage": "2.60",
-          "analysisTime": 19
-        },
-        "diffImage": "..\\bitmaps_test\\20221207-132920\\failed_diff_Big_search_bar_data-qa_big_720px.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Small_search_bar_data-qa_small_1024px.png",
-        "test": "..\\bitmaps_test\\20221207-132920\\Small_search_bar_data-qa_small_1024px.png",
+        "test": "..\\bitmaps_test\\20221209-113340\\Small_search_bar_data-qa_small_1024px.png",
         "selector": "[data-qa=\"small\"]",
         "fileName": "Small_search_bar_data-qa_small_1024px.png",
         "label": "Small search bar data-qa_small",
@@ -124,17 +116,15 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "1.37",
-          "analysisTime": 25
-        },
-        "diffImage": "..\\bitmaps_test\\20221207-132920\\failed_diff_Small_search_bar_data-qa_small_1024px.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Small_search_bar_data-qa_small_720px.png",
-        "test": "..\\bitmaps_test\\20221207-132920\\Small_search_bar_data-qa_small_720px.png",
+        "test": "..\\bitmaps_test\\20221209-113340\\Small_search_bar_data-qa_small_720px.png",
         "selector": "[data-qa=\"small\"]",
         "fileName": "Small_search_bar_data-qa_small_720px.png",
         "label": "Small search bar data-qa_small",
@@ -150,12 +140,10 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "1.96",
-          "analysisTime": 28
-        },
-        "diffImage": "..\\bitmaps_test\\20221207-132920\\failed_diff_Small_search_bar_data-qa_small_720px.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     }
   ]
 });
